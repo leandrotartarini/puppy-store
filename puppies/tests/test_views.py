98f1fa0f4ba113e2d88fb5bee0b26e3cd912c505
leandrotartarini <ldo.tartarini@gmail.com>
2021-12-01 07:19:24 +0000

    Adding put test

--- puppies/tests/test_views.py
+++ puppies/tests/test_views.py
@@ -1,7 +1,9 @@
 import json
+from django.http import response
 from rest_framework import status
 from django.test import TestCase, Client, client
 from django.urls import reverse
+import rest_framework
 from ..models import Puppy
 from ..serializers import PuppySerializer
 
@@ -78,6 +80,42 @@ class CreateNewPuppyTest(TestCase):
     def test_create_invalid_puppy(self):
         response = client.post(
             reverse('get_post_puppies'),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+
+class UpdateSinglePuppyTest(TestCase):
+    """ Test module for updating an existing puppy record """
+
+    def setUp(self):
+        self.casper = Puppy.objects.create(name='Casper', age=3, breed='Bull Dog', color='Black')
+        self.muffin = Puppy.objects.create(name='Muffin', age=1, breed='Gradane', color='Brown')
+        self.valid_payload = {
+            'name': 'Muffy',
+            'age': 2,
+            'breed': 'Labrador',
+            'color': 'Black',
+        }
+        self.invalid_payload = {
+            'name': '',
+            'age': 4,
+            'breed': 'Pamerion',
+            'color': 'White',
+        }
+    
+    def test_valid_update_puppy(self):
+        response = client.put(
+            reverse('get_delete_update_puppy', kwargs={'pk': self.muffin.pk}),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    
+    def test_invalid_update_puppy(self):
+        response = client.put(
+            reverse('get_delete_update_puppy', kwargs={'pk': self.muffin.pk}),
             data=json.dumps(self.invalid_payload),
             content_type='application/json'
         )
